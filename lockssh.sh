73cd2040a151b688b37e918225a938c1118383ec
andrew johnson <sleepdev@gmail.com>
2010-10-05 02:49:36 -0600

    First script complete.

--- lockssh.sh
+++ lockssh.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-$1 = first command line argument
+
+#requires that the user create a rsa public key at ~/.ssh/id_rsa.pub
+if [ ! -f ~/.ssh/id_rsa ]
+then 
+   echo 'save generated key file at ~/.ssh/id_rsa'
+   ssh-keygen -t rsa
+fi
+
+ssh root@$1 'mkdir ~/.ssh ; touch ~/.ssh/authorized_keys'
+cat ~/.ssh/id_rsa.pub | ssh root@$1 'cat >> ~/.ssh/authorized_keys'
+
+#based on solution from http://linuxproblem.org/art_9.html
